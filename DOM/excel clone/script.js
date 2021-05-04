657a7099b61b1637ca11009b1d83c167a291d488
bhavesh-pepcoding <bhavesh.bansal@pepcoding.com>
2021-05-04 21:42:35 +0530

    ADD v7

--- DOM/excel clone/script.js
+++ DOM/excel clone/script.js
@@ -44,58 +44,90 @@ $(".input-cell").blur(function () {
 });
 
 $(".input-cell").click(function (e) {
+    let idArray = $(this).attr("id").split("-");
+    let rowId = parseInt(idArray[1]);
+    let colId = parseInt(idArray[3]);
+    let topCell = $(`#row-${rowId - 1}-col-${colId}`);
+    let bottomCell = $(`#row-${rowId + 1}-col-${colId}`);
+    let leftCell = $(`#row-${rowId}-col-${colId - 1}`);
+    let rightCell = $(`#row-${rowId}-col-${colId + 1}`);
 
+    if ($(this).hasClass("selected")) {
+        unselectCell(this, e, topCell, bottomCell, leftCell, rightCell)
+    } else {
+        selectCell(this, e, topCell, bottomCell, leftCell, rightCell);
+    }
+
+});
+
+function unselectCell(ele, e, topCell, bottomCell, leftCell, rightCell) {
     if (e.ctrlKey) {
-        let idArray = $(this).attr("id").split("-");
+        if ($(ele).hasClass("top-selected")) {
+            topCell.removeClass("bottom-selected");
+        }
+        if ($(ele).hasClass("left-selected")) {
+            leftCell.removeClass("right-selected");
+        }
+        if ($(ele).hasClass("right-selected")) {
+            rightCell.removeClass("left-selected");
+        }
+        if ($(ele).hasClass("bottom-selected")) {
+            bottomCell.removeClass("top-selected");
+        }
+        $(ele).removeClass("selected top-selected bottom-selected right-selected left-selected");
+    }
+}
+
+function selectCell(ele, e, topCell, bottomCell, leftCell, rightCell) {
+    if (e.ctrlKey) {
+        let idArray = $(ele).attr("id").split("-");
         let rowId = parseInt(idArray[1]);
         let colId = parseInt(idArray[3]);
 
         // top selected or not
         let topSelected;
-        let topCell;
-        if (rowId != 0) {
-            topCell = $(`#row-${rowId - 1}-col-${colId}`);
+        if (topCell) {
             topSelected = topCell.hasClass("selected");
         }
         // bottom selected or not
-        let bottomCell = $(`#row-${rowId + 1}-col-${colId}`);
-        let bottomSelected = bottomCell.hasClass("selected");
-        console.log(bottomCell);
+        let bottomSelected;
+        if (bottomCell) {
+            bottomSelected = bottomCell.hasClass("selected");
+        }
 
         // left selected or not
-        let leftCell;
         let leftSelected;
-        if (colId != 0) {
-            leftCell = $(`#row-${rowId}-col-${colId - 1}`)
+        if (leftCell) {
             leftSelected = leftCell.hasClass("selected");
         }
         // right selected or not
-        let rightCell = $(`#row-${rowId}-col-${colId + 1}`)
-        let rightSelected = rightCell.hasClass("selected");
+        let rightSelected;
+        if (rightCell) {
+            rightSelected = rightCell.hasClass("selected");
+        }
 
         if (topSelected) {
             topCell.addClass("bottom-selected");
-            $(this).addClass("top-selected");
+            $(ele).addClass("top-selected");
         }
 
         if (leftSelected) {
             leftCell.addClass("right-selected");
-            $(this).addClass("left-selected");
+            $(ele).addClass("left-selected");
         }
 
         if (rightSelected) {
             rightCell.addClass("left-selected");
-            $(this).addClass("right-selected");
+            $(ele).addClass("right-selected");
         }
 
         if (bottomSelected) {
             bottomCell.addClass("top-selected");
-            $(this).addClass("bottom-selected");
+            $(ele).addClass("bottom-selected");
         }
     } else {
         $(".input-cell.selected").removeClass("selected top-selected bottom-selected right-selected left-selected");
     }
 
-    $(this).addClass("selected");
-
-})
+    $(ele).addClass("selected");
+}
