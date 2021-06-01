d64b8ae10a8708107f732ec21f45f00bbc5fda5f
bhavesh-pepcoding <bhavesh.bansal@pepcoding.com>
2021-06-01 20:24:09 +0530

    ADD v1

--- BOM/Actual App/script.js
+++ BOM/Actual App/script.js
@@ -1,6 +1,9 @@
-let constraints = {video: true};
+let constraints = { video: true };
 
 let videoPlayer = document.querySelector("video");
+
+let frame = document.querySelector(".frame");
+frame.style["max-width"] = videoPlayer.offsetWidth + "px";
 let vidRecordBtn = document.querySelector("#record-video");
 
 let captureBtn = document.querySelector("#click-picture");
@@ -8,13 +11,31 @@ let captureBtn = document.querySelector("#click-picture");
 let recordState = false;
 let chunks = [];
 let mediaRecorder;
+let zoom = 1;
 
-captureBtn.addEventListener("click", function() {
+
+let zoomIn = document.querySelector(".zoom-in");
+zoomIn.addEventListener("click", function () {
+    if (zoom < 2.5) {
+        zoom += 0.1;
+        videoPlayer.style.transform = `scale(${zoom})`;
+    }
+});
+
+let zoomOut = document.querySelector(".zoom-out");
+zoomOut.addEventListener("click", function () {
+    if (zoom > 1) {
+        zoom -= 0.1;
+        videoPlayer.style.transform = `scale(${zoom})`;
+    }
+})
+
+captureBtn.addEventListener("click", function () {
     capture();
 })
 
-vidRecordBtn.addEventListener("click",function(){
-    if(!recordState) {
+vidRecordBtn.addEventListener("click", function () {
+    if (!recordState) {
         mediaRecorder.start();
         recordState = true;
         vidRecordBtn.innerText = "Recording...";
@@ -25,16 +46,17 @@ vidRecordBtn.addEventListener("click",function(){
     }
 });
 
-navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream) {
+navigator.mediaDevices.getUserMedia(constraints).then(function (mediaStream) {
+    console.log(mediaStream.getVideoTracks()[0].getCapabilities());
     videoPlayer.srcObject = mediaStream;
     mediaRecorder = new MediaRecorder(mediaStream);
 
-    mediaRecorder.ondataavailable = function(e) {
+    mediaRecorder.ondataavailable = function (e) {
         chunks.push(e.data);
     }
 
-    mediaRecorder.onstop = function() {
-        let blob  = new Blob(chunks, {type: "video/mp4"});
+    mediaRecorder.onstop = function () {
+        let blob = new Blob(chunks, { type: "video/mp4" });
         chunks = [];
         let blobUrl = URL.createObjectURL(blob);
         let a = document.createElement("a");
@@ -48,12 +70,14 @@ function capture() {
     let canvas = document.createElement("canvas");
     canvas.width = videoPlayer.videoWidth;
     canvas.height = videoPlayer.videoHeight;
-    console.log(videoPlayer.videoWidth, videoPlayer.videoHeight);
     let ctx = canvas.getContext("2d");
-    ctx.drawImage(videoPlayer, 0 ,0);
-    if(filter != "") {
+    ctx.translate(canvas.width/2, canvas.height/2);
+    ctx.scale(zoom,zoom);
+    ctx.translate(-(canvas.width/2),-(canvas.height/2));
+    ctx.drawImage(videoPlayer, 0, 0);
+    if (filter != "") {
         ctx.fillStyle = filter;
-        ctx.fillRect(0,0, canvas.width, canvas.height);
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
     let link = document.createElement("a");
     link.download = "imgi.png";
@@ -66,8 +90,8 @@ let filter = "";
 
 let allFilters = document.querySelectorAll(".filter");
 
-for(let i of allFilters) {
-    i.addEventListener("click", function(e) {
+for (let i of allFilters) {
+    i.addEventListener("click", function (e) {
         filter = e.currentTarget.style.backgroundColor;
         addFilterToScreen(filter);
     })
@@ -75,7 +99,7 @@ for(let i of allFilters) {
 
 function addFilterToScreen(filter) {
     let prevScreenFilter = document.querySelector(".screen-filter");
-    if(prevScreenFilter) {
+    if (prevScreenFilter) {
         prevScreenFilter.remove();
     }
     let filterScreen = document.createElement("div");
@@ -85,3 +109,7 @@ function addFilterToScreen(filter) {
     filterScreen.style.backgroundColor = filter;
     document.querySelector(".filter-screen-parent").append(filterScreen);
 }
+
+navigator.mediaDevices.enumerateDevices().then(function (devices) {
+    console.log(devices);
+})
