47a823531b52be940d6c632cc9acd0092df85e5f
bhavesh-pepcoding <bhavesh.bansal@pepcoding.com>
2021-06-02 21:23:15 +0530

    ADD delete

--- BOM/Actual App/dbScript.js
+++ BOM/Actual App/dbScript.js
@@ -25,6 +25,7 @@ function getData() {
     let store = tx.objectStore("gallery");
     let req = store.openCursor();
     let gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
     req.onsuccess = function (e) {
         let cursor = req.result;
         if (cursor) {
@@ -33,29 +34,37 @@ function getData() {
                 image.classList.add("image");
                 image.innerHTML = `<img src='${cursor.value.data}'></img>
                 <div class="buttons">
-                    <button class="delete">Delete</button>
+                    <button class="delete${cursor.value.nId}">Delete</button>
                     <button class='download${cursor.value.nId}'>Download</button>
                 </div>`;
                 gallery.append(image);
                 let url = cursor.value.data;
                 let fileName = cursor.value.nId + ".png";
+                let nId = cursor.value.nId;
                 document.querySelector(`.download${cursor.value.nId}`).addEventListener("click", function(){
                     download(url,fileName);
-                })
+                });
+                document.querySelector(`.delete${cursor.value.nId}`).addEventListener("click", function(){
+                    deleteFromGallery(nId);
+                });
             } else {
                 let videoUrl = URL.createObjectURL(cursor.value.data);
                 let video = document.createElement("div");
                 video.classList.add("video");
                 video.innerHTML = `<video autoplay src='${videoUrl}' loop></video>
                 <div class="buttons">
-                    <button class="delete">Delete</button>
+                    <button class="delete${cursor.value.nId}">Delete</button>
                     <button class="download${cursor.value.nId}">Download</button>
                 </div>`;
                 gallery.append(video);
                 let fileName = cursor.value.nId + ".mp4";
+                let nId = cursor.value.nId;
                 document.querySelector(`.download${cursor.value.nId}`).addEventListener("click", function(){
                     download(videoUrl,fileName);
-                })
+                });
+                document.querySelector(`.delete${cursor.value.nId}`).addEventListener("click", function(){
+                    deleteFromGallery(nId);
+                });
             }
             cursor.continue();
         } else {
@@ -70,4 +79,11 @@ function download(url,name) {
     a.href = url;
     a.download = name;
     a.click();
+}
+
+function deleteFromGallery(nId) {
+    let tx = db.transaction("gallery","readwrite");
+    let store = tx.objectStore("gallery");
+    store.delete(Number(nId));
+    getData();
 }
